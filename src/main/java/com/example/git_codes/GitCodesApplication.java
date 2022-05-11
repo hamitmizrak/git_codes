07c73d03fca5ea0f41fa9d6187fa360a4b825cbe
hamitmizrak <hamitmizrak@gmail.com>
2022-05-11 21:27:00 +0300

    first commit

---
+++ src/main/java/com/example/git_codes/GitCodesApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_codes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCodesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitCodesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/git_codes/GitCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_codes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCodesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
